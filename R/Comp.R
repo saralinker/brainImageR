af5fc2e346ad88965833810da89e7dc208de62ce
Sara Linker <slinker@Saras-MacBook-Pro.local>
2018-05-30 07:32:05 -0700

    tried moving the project file back into the folder

--- R/Comp.R
+++ R/Comp.R
@@ -8,7 +8,7 @@
 #' @examples
 #' comp <- methods::new(Class="Comp",
 #' tissueExp1 = c(10,12),
-#' tissueExp2 =  c(10,12),
+#' tissueExp2 =  c(10,13),
 #' composite = matrix(0,nrow=10,ncol=10),
 #' random.matrix = data.frame(matrix(0,nrow=10,ncol=10)),
 #' refset = "developing"
