8f8aa7ad99ac1b988d26c7d858e34ce68d2556bc
Sara Linker <slinker@Saras-MacBook-Pro.local>
2018-05-30 07:08:09 -0700

    removed brainImager project file and updated Comp example

--- R/Comp.R
+++ R/Comp.R
@@ -8,7 +8,7 @@
 #' @examples
 #' comp <- methods::new(Class="Comp",
 #' tissueExp1 = c(10,12),
-#' tissueExp2 =  table(c(rep("A",5), rep("B",5))),
+#' tissueExp2 =  c(10,12),
 #' composite = matrix(0,nrow=10,ncol=10),
 #' random.matrix = data.frame(matrix(0,nrow=10,ncol=10)),
 #' refset = "developing"
